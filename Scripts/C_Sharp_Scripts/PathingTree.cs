4715a80e8d8932200e922d1fc35d8183f25031b6
Benjamin Rendell <benjamin.rendell@students.mq.edu.au>
2024-02-15 21:38:48 +1100

    Removed ShootRailOld() and UpdateGraphOld() functions.

--- Scripts/C_Sharp_Scripts/PathingTree.cs
+++ Scripts/C_Sharp_Scripts/PathingTree.cs
@@ -80,26 +80,18 @@ public partial class PathingTree : Node2D
 		Vector2 pointQ = pointA + pointAQ;
 		return pointQ;
 	}
-	
-	// OLD: Function that shoots a new rail at mouse position
-	public void ShootRailOld(float dist){
-		Vector2 TargetRangeLimit = CalculateRange(Railshooter.Position, mousePosition, dist); //Applies a range limit to the Railshooter's gun.
-		generatedNode = GenerateNode(TargetRangeLimit);
-		UpdateGraphOld(generatedNode);
-		render.DrawRailLines(this, GetTarget(), GetGeneratedNode());	//renders rail sprites using Line2D nodes
-	}
 
 	//Function that shoots a new rail at mouse position
 	public void ShootRail(float dist){
-			Vector2 TargetRangeLimit = CalculateRange(Railshooter.Position, mousePosition, dist); //Applies a range limit to the Railshooter's gun.
-			if(ray.GetCollisionState() < 0){
-				generatedNode = GenerateNode(TargetRangeLimit);
-			}else{
-				generatedNode = nodes[ray.GetCollisionState()];
-			}
-			UpdateGraph(GetTarget(), GetGeneratedNode());
-			render.DrawRailLines(this, GetTarget(), GetGeneratedNode());	//renders rail sprites using Line2D nodes
+		Vector2 TargetRangeLimit = CalculateRange(Railshooter.Position, mousePosition, dist); //Applies a range limit to the Railshooter's gun.
+		if(ray.GetCollisionState() < 0){
+			generatedNode = GenerateNode(TargetRangeLimit);
+		}else{
+			generatedNode = nodes[ray.GetCollisionState()];
 		}
+		UpdateGraph(GetTarget(), GetGeneratedNode());
+		render.DrawRailLines(this, GetTarget(), GetGeneratedNode());	//renders rail sprites using Line2D nodes
+	}
 
 	//Create a new Node2D in scene at target location. https://www.youtube.com/watch?v=yxin2ScRnMU
 	public Node2D GenerateNode(Vector2 pos){
@@ -114,25 +106,6 @@ public partial class PathingTree : Node2D
 
 		nodes.Add(newArea);		//Adds the newly generated Line2D node to the 'nodes' Godot Array
 		return newArea;
-	}
-
-	//OLD: Add a new node to Dictionaries.
-	public void UpdateGraphOld(Node2D node){
-		//check if node is valid
-		if(nodes.Contains(node) && nodes.Contains(GetTarget())){
-			//get node index in array
-			int newIndex = nodes.IndexOf(node);
-
-			//Add new node index to graph_ptr_dict with relation array set to empty
-			Godot.Collections.Array<int> newRelation = new();
-			graph_ptr_dict.Add(newIndex, newRelation);
-
-			//access Target's relation array in graph_ptr_dict and add the new index.
-			int targetIndex = nodes.IndexOf(GetTarget());
-			graph_ptr_dict[targetIndex].Add(newIndex);
-
-			SetGraphUpdateState(true);	//update graph state to prevent rerunning procedure.
-		}
 	}
 
 	//Update the relation between 2 nodes in the dictionary
